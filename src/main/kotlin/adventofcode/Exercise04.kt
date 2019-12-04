6f570c76e98dc6635e14981da9f2ab4599ce9361
Remco Weekers <rweekers@bol.com>
2019-12-04 20:46:57 +0100

    Exercise 04

--- src/main/kotlin/adventofcode/Exercise04.kt
+++ src/main/kotlin/adventofcode/Exercise04.kt
@@ -1,22 +1,37 @@
 package adventofcode
 
-class Exercise04(input: String) {
+class Exercise04 {
 
-    private val inputList = resourceAsListOfString(input)
+    private val start = 171309
+    private val end = 643603
+
+    private val input = (start..end)
+            .map { it.toListOfInt() }
 
     fun silverExercise(): Int {
-        inputList.forEach { println(it) }
-        return 0
+        return input
+                .filter { allDigitsIncreasingOrEqual(it) && hasAtLeastTwoSubsequentEqualDigits(it) }.count()
     }
 
+    fun hasAtLeastTwoSubsequentEqualDigits(input: List<Int>): Boolean =
+            input
+                    .zipWithNext { a, b -> a == b }
+                    .any { it }
+
+    fun hasExactlyTwoSubsequentEqualDigits(input: List<Int>): Boolean =
+            (0..9)
+                    .any { input.indexOf(it) == input.lastIndexOf(it) - 1 }
+
+    fun allDigitsIncreasingOrEqual(input: List<Int>): Boolean =
+            (0..4).all { input[it] <= input[it + 1] }
+
     fun goldExercise(): Int {
-        inputList.forEach { println(it) }
-        return 0
+        return input.filter { allDigitsIncreasingOrEqual(it) && hasExactlyTwoSubsequentEqualDigits(it) }.count()
     }
 }
 
 fun main() {
-    val exc4 = Exercise04("/input04.txt")
+    val exc4 = Exercise04()
     println("The answer to the silver exercise is ${exc4.silverExercise()}")
     println("The answer to the gold exercise is ${exc4.goldExercise()}")
 }
