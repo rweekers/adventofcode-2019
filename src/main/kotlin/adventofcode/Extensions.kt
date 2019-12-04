b9bc1760f726007ec5fc1e5959e5b80f1b4c56c6
Remco Weekers <rweekers@bol.com>
2019-12-04 20:46:57 +0100

    Exercise 04

--- src/main/kotlin/adventofcode/Extensions.kt
+++ src/main/kotlin/adventofcode/Extensions.kt
@@ -3,3 +3,7 @@ package adventofcode
 fun <T> List<T>.innerJoin(otherList: List<T>): List<T> {
     return this.filter { otherList.contains(it) }
 }
+
+fun Int.toListOfInt(): List<Int> {
+    return this.toString().map { it.toString().toInt() }
+}
--- src/test/kotlin/adventofcode/Exercise04Test.kt
+++ src/test/kotlin/adventofcode/Exercise04Test.kt
@@ -7,13 +7,27 @@ class Exercise04Test {
 
     @Test
     fun `exercise 4 silver test`() {
-        val exc4 = Exercise04("/test04.txt")
-        assertThat(exc4.silverExercise()).isEqualTo(0)
+        val exc4 = Exercise04()
+        assertThat(exc4.hasAtLeastTwoSubsequentEqualDigits(111111.toListOfInt())).isEqualTo(true)
+        assertThat(exc4.allDigitsIncreasingOrEqual(111111.toListOfInt())).isEqualTo(true)
+
+        assertThat(exc4.hasAtLeastTwoSubsequentEqualDigits(223450.toListOfInt())).isEqualTo(true)
+        assertThat(exc4.allDigitsIncreasingOrEqual(223450.toListOfInt())).isEqualTo(false)
+
+        assertThat(exc4.hasAtLeastTwoSubsequentEqualDigits(123789.toListOfInt())).isEqualTo(false)
+        assertThat(exc4.allDigitsIncreasingOrEqual(123789.toListOfInt())).isEqualTo(true)
     }
 
     @Test
     fun `exercise 4 gold test`() {
-        val exc4 = Exercise04("/test04.txt")
-        assertThat(exc4.goldExercise()).isEqualTo(0)
+        val exc4 = Exercise04()
+        assertThat(exc4.hasExactlyTwoSubsequentEqualDigits(112233.toListOfInt())).isEqualTo(true)
+        assertThat(exc4.allDigitsIncreasingOrEqual(112233.toListOfInt())).isEqualTo(true)
+
+        assertThat(exc4.hasExactlyTwoSubsequentEqualDigits(123444.toListOfInt())).isEqualTo(false)
+        assertThat(exc4.allDigitsIncreasingOrEqual(123444.toListOfInt())).isEqualTo(true)
+
+        assertThat(exc4.hasExactlyTwoSubsequentEqualDigits(111122.toListOfInt())).isEqualTo(true)
+        assertThat(exc4.allDigitsIncreasingOrEqual(111122.toListOfInt())).isEqualTo(true)
     }
 }
