6ec9ba067452c28d6d646d3328e4814c44c5038d
Remco Weekers <rweekers@bol.com>
2019-12-06 22:37:00 +0100

    Exercise 5

--- src/main/kotlin/adventofcode/Exercise05.kt
+++ src/main/kotlin/adventofcode/Exercise05.kt
@@ -3,60 +3,14 @@ package adventofcode
 class Exercise05(input: String) {
 
     private val initialList = lineAsListOfInts(input)
-    private var inputList: MutableList<Int> = mutableListOf()
-    private var parameter = 1
+    private val intCode = IntCode(initialList)
 
     fun silverExercise(): Int {
-        inputList.forEach { println(it) }
-        runProgram()
-        return 0
-    }
-
-    private fun runProgram() {
-        inputList = initialList.toMutableList()
-        var currentIndex = 0
-        do {
-            currentIndex += processChunk(currentIndex)
-        } while (inputList[currentIndex] != 99)
-    }
-
-    private fun processChunk(currentIndex: Int): Int {
-        val code = MemCodeInstruction.getByCode(inputList[currentIndex].toString().takeLast(2).toInt())
-
-        when (code) {
-            MemCodeInstruction.INPUT -> println("TODO")
-            MemCodeInstruction.OUTPUT -> println("TOOD")
-            MemCodeInstruction.ADD -> println("TODO")
-            MemCodeInstruction.MULTIPLY -> println("TODO")
-            MemCodeInstruction.END -> println("TODO")
-        }
-
-        return currentIndex + code.length
-    }
-
-    private class Chunk(val memCodeInstruction: MemCodeInstruction, val indexOperandOne: Int, val indexOperandTwo: Int, val targetIndex: Int)
-
-    private enum class MemCodeInstruction(val code: Int, val length: Int) {
-        INPUT(3, 1),
-        OUTPUT(4, 1),
-        ADD(1, 4),
-        MULTIPLY(2, 4),
-        END(99, 1);
-
-        companion object {
-            fun getByCode(code: Int): MemCodeInstruction {
-                for (memCodeInstruction in MemCodeInstruction.values()) {
-                    if (memCodeInstruction.code == code) {
-                        return memCodeInstruction
-                    }
-                }
-                throw RuntimeException("MemCodeInstruction with code $code not found.")
-            }
-        }
+        return intCode.runProgram()
     }
 
     fun goldExercise(): Int {
-        return 0
+        return intCode.runProgram(5)
     }
 }
 
